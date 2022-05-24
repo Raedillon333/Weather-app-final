d670b2bc29a1602673850d8ecc353c3e4376cf56
Raedillon333 <105652730+Raedillon333@users.noreply.github.com>
2022-05-24 15:07:48 +0200

    showing the current temperature in js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#windspeed");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "9351a98b93b6c78e31043ebe9fd5f6e5";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
